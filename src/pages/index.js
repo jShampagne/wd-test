33eda7a4dff05180445efe4655b0a387fda4499e
Jacob Shampine <jacob.shampine@offmadisonave.com>
2019-05-09 09:43:40 -0700

    We can do this

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Let's hope this works!</h1>
     <p>Jake is the coolest</p>
-    <p>Now go build something great.</p>
+    <p>We can do this!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
